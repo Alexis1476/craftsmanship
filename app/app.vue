19c7424b0f3d1c8780b70cab1403c9af9765111d
Alexis1476 <halexisr20@hotmail.com>
2026-02-13 20:39:07 +0100

    chore: install tailwind and nuxtui

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <NuxtPage />
+    <main class="flex flex-col items-center justify-center min-h-screen">
+      <h1 class="text-4xl">Hello World</h1>
+    </main>
+  </UApp>
 </template>
